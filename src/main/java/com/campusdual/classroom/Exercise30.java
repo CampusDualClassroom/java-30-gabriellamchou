ab12d53a6d8627a99d315ba3781d08dace0c11b2
Gabriel <gabriel.lamela.ch@gmail.com>
2024-11-28 20:38:40 +0100

    Ejercicio 30 resuelto

--- src/main/java/com/campusdual/classroom/Exercise30.java
+++ src/main/java/com/campusdual/classroom/Exercise30.java
@@ -2,11 +2,20 @@ package com.campusdual.classroom;
 
 public class Exercise30 {
 
-    public static int divisionWithCustomException(int dividend, int divisor){
-
+    public static int divisionWithCustomException(int dividend, int divisor) throws DivisionByZeroException {
+        if (divisor == 0){
+            throw new DivisionByZeroException("Error: Has intentado dividir por cero.");
+        }
+        return dividend / divisor;
     }
 
     public static void main(String[] args) {
-
+        int result;
+        try {
+            result = divisionWithCustomException(3, 0);
+            System.out.println("Resultado: " + result);
+        } catch (DivisionByZeroException e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
